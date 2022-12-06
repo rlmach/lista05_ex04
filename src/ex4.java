8ba04137d8743e468f1b8b585e1f76ce285ccdc6
Rafael <machiavelli.rafa@gmail.com>
2022-12-06 19:43:56 -0300

    Exercício 4

---
+++ src/ex4.java
@@ -0,0 +1,16 @@
+public class ex4 {
+
+    public static void main(String[] args) {
+
+        if(tipoNumero(-8) < 0) {
+            System.out.println("n");
+        }
+        else {
+            System.out.println("P");
+        }
+    }
+
+    static int tipoNumero(int n) {
+        return n;
+    }
+}
